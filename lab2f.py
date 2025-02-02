460c7f5d4a942ae654a55c06dbf7d62af89be5c2
ralphtolentino16 <ralph.tolentino16@gmail.com>
2025-02-02 00:19:20 -0500

    Added lab2f

---
+++ lab2f.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+#Author: Ralph Louisse Tolentino
+#Author ID: 153087226
+#Date Created: 2025/02/02"
+
+import sys
+
+timer = int(sys.argv[1])
+while timer != 0:
+    print(timer)
+    timer = timer - 1
+print('blast off!')
